63c9bdae92a4ac19516fcc051231200c9f50ece6
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-12 01:35:10 +0000

    Automatically backed up by Learn

--- lib/practice.rb
+++ lib/practice.rb
@@ -6,14 +6,19 @@ def load_library(file_path)
   new_hash = {"get_meaning": {}, "get_emoticon": {}}
   
   file.each do |key,value|
+<<<<<<< HEAD
     new_hash[:get_meaning].store(value[1],key)
     new_hash[:get_emoticon].store(value[0],value[1])
+=======
+    new_hash[:get_meaning] = value[1]
+>>>>>>> 3c8ca0b3ce4f794b479bfc5b3e5338fb7fb20a70
   end
   
  puts new_hash
   
 end
 
+<<<<<<< HEAD
 def get_japanese_emoticon(file_path, emoticon)
   loaded = load_library(file_path)
 
@@ -24,6 +29,10 @@ def get_japanese_emoticon(file_path, emoticon)
       puts value
     end
   end
+=======
+def get_japanese_emoticon
+  # code goes here
+>>>>>>> 3c8ca0b3ce4f794b479bfc5b3e5338fb7fb20a70
 end
 
 def get_english_meaning
--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,12 @@ def load_library(file_path)
   new_hash = {"get_meaning": {}, "get_emoticon": {}}
   
   file.each do |key,value|
+<<<<<<< HEAD
     new_hash[:get_meaning].store(value[1],key)
     new_hash[:get_emoticon].store(value[0],value[1])
+=======
+    new_hash[:get_meaning].store(value,key)
+>>>>>>> 3c8ca0b3ce4f794b479bfc5b3e5338fb7fb20a70
   end
   
  new_hash
@@ -28,6 +32,7 @@ def get_japanese_emoticon(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
+<<<<<<< HEAD
 def get_english_meaning(file_path, emoticon)
   loaded = load_library(file_path)
 
@@ -41,3 +46,10 @@ def get_english_meaning(file_path, emoticon)
   
   return "Sorry, that emoticon was not found"
 end
+=======
+def get_english_meaning
+  # code goes here
+end
+
+load_library("./lib/emoticons.yml")
+>>>>>>> 3c8ca0b3ce4f794b479bfc5b3e5338fb7fb20a70
